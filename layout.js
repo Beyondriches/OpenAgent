256f7b77217b3c6651ca1b40ea35f159bf49a0c1
Beyondriches <ray.faour@hotmail.com>
2026-09-15 11:38:46 +0200

    Delete layout.js

--- layout.js
+++
@@ -1,2 +0,0 @@
-export const metadata = { title: 'Theo Crypto Agent', description: 'Crypto intelligence cockpit' };
-export default function RootLayout({ children }) { return <html lang="en"><body style={{margin:0,background:'#0b1117',color:'#eef4fa'}}>{children}</body></html>; }
