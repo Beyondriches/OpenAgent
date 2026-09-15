5c3b2d3ad98f6fb016926c796c8bc2eddc48d8f3
Beyondriches <ray.faour@hotmail.com>
2026-09-15 11:39:12 +0200

    Delete route.js

--- route.js
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  return Response.json({
-    ok: true,
-    agent: 'Theo Crypto Agent',
-    version: '0.2',
-    mode: 'paper-trading',
-    message: 'API route is working.'
-  });
-}
