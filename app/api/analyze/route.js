a39c1b2c60bae704bb671634d6ed2478ec2f859f
Beyondriches <ray.faour@hotmail.com>
2026-09-16 12:06:19 +0200

    v1.7 increase CoinGecko cache duration

--- app/api/analyze/route.js
+++ app/api/analyze/route.js
@@ -871,7 +871,7 @@ export async function POST(request) {
           headers: {
             accept: "application/json",
           },
-          next: { revalidate: 60 },
+          next: { revalidate: 300 },
         }),
 
         fetchWithRetry(historyUrl, {
